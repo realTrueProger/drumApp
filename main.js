c05a49cda4d81e9d9c9f51604a1ab6d7f634e4fa
trueProger <vovasollo@yandex.ru>
2018-03-13 13:41:47 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+function play(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function playClick(e) {
+    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"`);
+    const key = document.querySelector(`.key[data-key="${this.dataset.key}"`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', playClick));
+window.addEventListener('keydown', play);
