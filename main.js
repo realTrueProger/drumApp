eb84ed655d66ef54260b6fa83b63c57fcf189bea
trueProger <vovasollo@yandex.ru>
2018-03-13 14:41:35 +0300

    fix bugs

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function removeTransition(e) {
 }
 
 function play(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
@@ -13,16 +13,17 @@ function play(e) {
 }
 
 function playClick(e) {
-    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"`);
-    const key = document.querySelector(`.key[data-key="${this.dataset.key}"`);
+    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
+    const key = document.querySelector(`.key[data-key="${this.dataset.key}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
 }
 
+
+
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-//keys.forEach(key => key.addEventListener('click', playClick));
-keys.forEach(key => key.addEventListener('touchend', playClick));
+keys.forEach(key => key.addEventListener('click', playClick));
 window.addEventListener('keydown', play);
